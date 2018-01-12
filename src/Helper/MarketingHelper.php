42fa79edaeba758d3d2965f1dc7e234e5281db9e
yangshuai <yangshuai@howzhi.com>
2018-01-12 17:18:02 +0800

    delete unused method

--- src/Helper/MarketingHelper.php
+++ src/Helper/MarketingHelper.php
@@ -4,21 +4,6 @@ namespace QiQiuYun\SDK\Helper;
 
 class MarketingHelper
 {
-    public static function transformStudent($items)
-    {
-        $students = array();
-        foreach ($items as $item) {
-            $student['user_source_id'] = $item['id'];
-            $student['nickname'] = $item['nickname'];
-            $student['mobile'] = isset($item['mobile']) ? $item['mobile'] : '';
-            $student['registered_time'] = $item['createdTime'];
-            $student['token'] = $item['token'];
-            $students[] = $student;
-        }
-
-        return $students;
-    }
-
     public static function generateLoginForm($action, $user, $site, $sign)
     {
         $loginTitle = "<div align='center'> 正在登陆中......</div>";
