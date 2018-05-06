aa1afd930ad0f50adad3df02bb45b3da6fd41eca
IlhamTahir <yantaq@bilig.biz>
2018-05-07 05:37:11 +0800

    feat: add MpService

---
+++ src/Service/MpService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QiQiuYun\SDK\Service;
+
+class MpService extends BaseService
+{
+    protected $host = 'mp-platform-dev.cn';
+
+    public function sendMpRequest(array $params)
+    {
+        return $this->request('POST', '/mpRequests', $params);
+    }
+
+    public function getCurrentMpRequest()
+    {
+        return $this->request('GET', '/mpRequests/current', array());
+    }
+}
