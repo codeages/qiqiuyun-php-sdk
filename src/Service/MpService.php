d327c25d89ba1a4f9b3ea1234d9a95a5ff341e4a
IlhamTahir <yantaq@bilig.biz>
2018-07-04 14:44:33 +0800

    fix: 去除多余接口

--- src/Service/MpService.php
+++ src/Service/MpService.php
@@ -6,16 +6,6 @@ class MpService extends BaseService
 {
     protected $host = 'mp-service.qiqiuyun.net';
 
-    public function sendMpRequest(array $params)
-    {
-        return $this->request('POST', '/mpRequests', $params);
-    }
-
-    public function getCurrentMpRequest()
-    {
-        return $this->request('GET', '/mpRequests/current', array());
-    }
-
     public function getToken(array $params)
     {
         return $this->request('POST', '/tokens', $params);
