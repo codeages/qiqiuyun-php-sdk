86c2429cbe89e1e0b39d9bd69e1c22929ed4346a
ruei <wurui@edusoho.com>
2019-11-28 20:09:13 +0800

    report success order

--- src/Service/S2B2CService.php
+++ src/Service/S2B2CService.php
@@ -15,4 +15,26 @@ class S2B2CService extends BaseService
         );
         return $this->request('GET', '/suppliers/actions/has_merchant', $query);
     }
+
+    /**
+     * @param $merchantAccessKey string 渠道商的 access_key
+     *
+     * @param $productIds array 订单中，供货商商品的 id，非本地商品 id.
+     * @example array(1,2,3);
+     *
+     * @param $extra array 上报的订单信息
+     * @example array('order' => $order, 'orderItems' => $orderItems)
+     *
+     * @return array
+     */
+    public function reportSuccessOrder($merchantAccessKey, $productIds, $extra)
+    {
+        $params = array(
+            'merchantAccessKey' => $merchantAccessKey,
+            'productIds' => $productIds,
+            'extra' => $extra,
+        );
+
+        return $this->request('POST', '/order/report/success', $params);
+    }
 }
