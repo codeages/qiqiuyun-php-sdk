a7f2801491562032c393a213f7b2d32e98b65678
zhanggaosheng <245085167@howzhi.com>
2019-12-15 17:03:02 +0800

    增加resource get update search delete getDownloadUrl

--- src/Service/ResourceService.php
+++ src/Service/ResourceService.php
@@ -2,6 +2,10 @@
 
 namespace QiQiuYun\SDK\Service;
 
+use QiQiuYun\SDK\Exception\ResponseException;
+use QiQiuYun\SDK\Exception\SDKException;
+use QiQiuYun\SDK\HttpClient\ClientException;
+
 class ResourceService extends BaseService
 {
     protected $host = 'resource-service.qiqiuyun.net';
@@ -11,6 +15,9 @@ class ResourceService extends BaseService
      *
      * @param $params array 参数
      * @return array 上传表单参数
+     * @throws ResponseException
+     * @throws SDKException
+     * @throws ClientException
      */
     public function startUpload(array $params)
     {
@@ -22,9 +29,74 @@ class ResourceService extends BaseService
      *
      * @param $no string 云资源编号
      * @return array
+     * @throws ClientException
+     * @throws ResponseException
+     * @throws SDKException
      */
-    public function finishUpload($no)
+    public function finishUpload(string $no)
     {
         return $this->request('POST', '/upload/finish', array('no' => $no));
+    }
+
+    /**
+     * @param string $no 云资源编号
+     * @return array
+     * @throws ClientException
+     * @throws ResponseException
+     * @throws SDKException
+     */
+    public function get(string $no)
+    {
+        return $this->request('GET', '/resources/' . $no);
+    }
+
+    /**
+     * @param array $params
+     * @return array
+     * @throws ClientException
+     * @throws ResponseException
+     * @throws SDKException
+     */
+    public function search(array $params)
+    {
+        return $this->request('GET', '/resources', $params);
+    }
+
+    /**
+     * @param string $no
+     * @param array $params
+     * @return string
+     * @throws ClientException
+     * @throws ResponseException
+     * @throws SDKException
+     */
+    public function getDownloadUrl(string $no, array $params = array())
+    {
+        return $this->request('GET', '/resources/' . $no . '/downloadUrl', $params);
+    }
+
+    /**
+     * @param string $no
+     * @param array $params
+     * @return array
+     * @throws ClientException
+     * @throws ResponseException
+     * @throws SDKException
+     */
+    public function update(string $no, array $params)
+    {
+        return $this->request('PATCH', '/resources/' . $no, $params);
+    }
+
+    /**
+     * @param string $no
+     * @return array
+     * @throws ClientException
+     * @throws ResponseException
+     * @throws SDKException
+     */
+    public function delete(string $no)
+    {
+        return $this->request('DELETE', '/resources/' . $no);
     }
 }
