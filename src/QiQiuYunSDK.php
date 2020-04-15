46e05c509ca3b94943c23e95f34896bb341b2ff4
钟云昶 <zhongyunchang@zhongyunchangdeMacBook-Pro-2.local>
2020-04-15 17:57:03 +0800

    增加S2BsCService

--- src/QiQiuYunSDK.php
+++ src/QiQiuYunSDK.php
@@ -139,6 +139,14 @@ class QiQiuYunSDK
     }
 
     /**
+     * @return \QiQiuYun\SDK\Service\S2B2CService
+     */
+    public function getS2B2CService()
+    {
+        return $this->getService('S2B2C');
+    }
+
+    /**
      * 根据服务名获得服务实例
      *
      * @param string $name 服务名
